c2c909d4063c7ee43c8d4bff61e530df14d52e0d
Ian Lai <ian.explore.world@gmail.com>
2018-04-21 17:55:08 +0000

    Modify the seedDatabase and clearDatabase to be async

--- dbHandler.js
+++ dbHandler.js
@@ -11,98 +11,155 @@ var adSchema = new mongoose.Schema({
 });
 var Ad = mongoose.model("Ad", adSchema);  
 //============================================================
-function seedDatabase(){
-    Ad.create({
+var obj1 = {
       img: "/sample1.png",
       url: "www.ad.com/1",
       ip: "101.0.0.1",
       timeStart: 1523963781963, 
       timeDuration: 1000000
-    }, function(err, saved){
-      if(err){
-          console.log("DB error");
-      }else{
-          console.log("DB is seeded - 1.");
-      }
-    });
-    
-    Ad.create({
+    };
+var obj2 = {
       img: "/sample2.png",
       url: "www.ad.com/2",
       ip: "102.0.0.1",
       timeStart: 1588863781963, 
       timeDuration: 800000000
-    }, function(err, saved){
-      if(err){
-          console.log("DB error");
-      }else{
-          console.log("DB is seeded - 2.");
-      }
-    });
-    
-    Ad.create({
+    };
+var obj3 = {
       img: "/sample3.png",
       url: "www.ad.com/3",
       ip: "103.0.0.1",
       timeStart: 1423980600000,
       timeDuration: 180000000000,
-    }, function(err, saved){
-      if(err){
-          console.log("DB error");
-      }else{
-          console.log("DB is seeded - 3.");
-      }
-    });
-    
-    Ad.create({
+    };
+var obj4 = {
       img: "/sample4.png",
       url: "www.ad.com/4",
       ip: "104.0.0.1",
       timeStart: 1500965447538, 
       timeDuration: 10000000
-    }, function(err, saved){
-      if(err){
-          console.log("DB error");
-      }else{
-          console.log("DB is seeded - 4.");
-      }
-    });
-
-    Ad.create({
+    };
+var obj5 = {
       img: "/sample5.png",
       url: "www.ad.com/5",
       ip: "105.0.0.1",
       timeStart: 1523265447538, 
       timeDuration: 8900000000
-    }, function(err, saved){
-      if(err){
-          console.log("DB error");
-      }else{
-          console.log("DB is seeded - 5.");
-      }
-    });
-    
-    Ad.create({
+    };
+var obj6 = {
       img: "/sample6.png",
       url: "www.ad.com/6",
       ip: "106.0.0.1",
       timeStart: 1523962447538, 
       timeDuration: 6700000000
-    }, function(err, saved){
-      if(err){
-          console.log("DB error");
-      }else{
-          console.log("DB is seeded - 6.");
-      }
+    };
+function seedDatabase(callback) {
+    Ad.create(obj1, function(err, saved) {
+        if (err) {
+            console.log("DB error");
+        } else {
+            console.log("DB is seeded - 1.");
+            Ad.create(obj2, function(err, saved) {
+                if (err) {
+                    console.log("DB error");
+                } else {
+                    console.log("DB is seeded - 2.");
+                    Ad.create(obj3, function(err, saved) {
+                        if (err) {
+                            console.log("DB error");
+                        } else {
+                            console.log("DB is seeded - 3.");
+                            Ad.create(obj4, function(err, saved) {
+                                if (err) {
+                                    console.log("DB error");
+                                } else {
+                                    console.log("DB is seeded - 4.");
+                                    Ad.create(obj5, function(err, saved) {
+                                        if (err) {
+                                            console.log("DB error");
+                                        } else {
+                                            console.log("DB is seeded - 5.");
+                                            Ad.create(obj6, function(err, saved) {
+                                                if (err) {
+                                                    console.log("DB error");
+                                                } else {
+                                                    console.log("DB is seeded - 6.");
+                                                    if(callback!==undefined){
+                                                      callback();
+                                                    }
+                                                }
+                                            });
+                                        }
+                                    });
+                                }
+                            });
+                        }
+                    });
+                }
+            });
+        }
     });
 }
 
-function clearDatabase(){
+// function seedDatabase(callback){
+//     Ad.create(obj1, function(err, saved){
+//       if(err){
+//           console.log("DB error");
+//       }else{
+//           console.log("DB is seeded - 1.");
+//       }
+//     });
+    
+//     Ad.create(obj2, function(err, saved){
+//       if(err){
+//           console.log("DB error");
+//       }else{
+//           console.log("DB is seeded - 2.");
+//       }
+//     });
+    
+//     Ad.create(obj3, function(err, saved){
+//       if(err){
+//           console.log("DB error");
+//       }else{
+//           console.log("DB is seeded - 3.");
+//       }
+//     });
+    
+//     Ad.create(obj4, function(err, saved){
+//       if(err){
+//           console.log("DB error");
+//       }else{
+//           console.log("DB is seeded - 4.");
+//       }
+//     });
+
+//     Ad.create(obj5, function(err, saved){
+//       if(err){
+//           console.log("DB error");
+//       }else{
+//           console.log("DB is seeded - 5.");
+//       }
+//     });
+    
+//     Ad.create(obj6, function(err, saved){
+//       if(err){
+//           console.log("DB error");
+//       }else{
+//           console.log("DB is seeded - 6.");
+//       }
+//     });
+// }
+
+function clearDatabase(callback){
     Ad.remove({}, function(err){
        if(err){
-           console.log(err);
+            console.log(err);
        }else{
-           console.log("DB is cleared.");
+            console.log("DB is cleared.");
+            if(callback!==undefined){
+              callback();
+            }
        }
     });
 }
